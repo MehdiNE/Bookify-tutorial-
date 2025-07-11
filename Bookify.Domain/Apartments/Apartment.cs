9bb78de834e5d9add7712ce07eca4c63caee18e3
MehdiNe <mahdimahdi9731@gmail.com>
2025-07-11 06:00:18 +0330

    Add domain layer

--- Bookify.Domain/Apartments/Apartment.cs
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace Bookify.Domain.Apartments
 {
-    internal class Apartment
+    public sealed class Apartment : Entity
     {
+        public Apartment(
+            Guid id,
+            Name name,
+            Description description,
+            Address address,
+            Money price,
+            Money cleaningFee,
+            List<Amenity> amenities)
+            : base(id)
+        {
+            Name = name;
+            Description = description;
+            Address = address;
+            Price = price;
+            CleaningFee = cleaningFee;
+            Amenities = amenities;
+        }
+
+        public Name Name { get; private set; }
+
+        public Description Description { get; private set; }
+
+        public Address Address { get; private set; }
+
+        public Money Price { get; private set; }
+
+        public Money CleaningFee { get; private set; }
+
+        public DateTime? LastBookedOnUtc { get; internal set; }
+
+        public List<Amenity> Amenities { get; private set; } = [];
     }
 }
